8f6b055a58de33458b93b0133be5406b08e8fa07
Wilhelm Gonce <goncequintanawilhelm@gmail.com>
2024-11-14 15:10:51 -0700

    join players test

--- Logic/Game.cs
+++ Logic/Game.cs
@@ -1,4 +1,6 @@
-﻿public class Game
+﻿using System.Security.AccessControl;
+
+public class Game
 {
     public GameStatus Status { get; private set; }
     public string Name { get; set; }
@@ -9,6 +11,32 @@
     public Player Player1 { get; private set; }
     public Player Player2 { get; private set; }
     public List<Tile> Board { get; private set; }
+    public bool IsGameOver
+    {
+        get
+        {
+            if (Player1 == null || Player2 == null)
+            {
+                return false;
+            }
+            if (Player1.Tiles.Count == 0 || Player2.Tiles.Count == 0 || NoOneCanPlay)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public bool IsPlayable
+    {
+        get
+        {
+            if (Player1 != null && Player2 != null && !IsGameOver)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
     public bool NoOneCanPlay
     {
         get
@@ -21,8 +49,37 @@
             return !player1CanPlay && !player2CanPlay;
         }
     }
+    public event Action? GameReset;
+    public event Action? GameStateChanged;
+
     public Game()
     {
+        Reset();
         Status = GameStatus.Not_Started;
+    }
+
+    public void Reset()
+    {
+        Player1 = null;
+        Player2 = null;
+        Board = new List<Tile> { new Tile(1, 1) };
+        GameReset?.Invoke();
+    }
+
+    public void Join(Player player)
+    {
+        if (Player1 is null)
+        {
+            Player1 = player;
+        }
+        else if (Player2 is null)
+        {
+            Player2 = player;
+        }
+        else
+        {
+            throw new Exception();//GameFullException();
+        }
+        GameStateChanged?.Invoke();
     }
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -11,4 +11,35 @@ public class UnitTest1
         var t2 = new Tile(2, 1);
         t1.Equals(t2).Should().BeTrue();
     }
+
+    [Fact]
+    public void NewGameInstanceFalse()
+    {
+        Game testGame = new Game();
+        testGame.IsGameOver.Should().Be(false);
+    }
+    [Fact]
+    public void NewGameInstanceIsPlayable()
+    {
+        Game testGame = new Game();
+        testGame.IsPlayable.Should().Be(false);
+    }
+    [Fact]
+    public void JoinOnePlayer()
+    {
+        Game testGame = new Game();
+        testGame.Join(new Player("test1"));
+        testGame.IsPlayable.Should().Be(false);
+    }  
+    [Fact]
+    public void JoinTwoPlayers()
+    {
+        Game testGame = new Game();
+        testGame.Join(new Player("test1"));
+        testGame.Join(new Player("test2"));
+        testGame.IsPlayable.Should().Be(true);
+        testGame.IsGameOver.Should().Be(false);
+    }  
+
+
 }
