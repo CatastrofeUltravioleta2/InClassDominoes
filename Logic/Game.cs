1d9405b8a751260f61eb4c8cf4120cebe259ec31
Wilhelm Gonce <goncequintanawilhelm@gmail.com>
2024-11-14 14:41:10 -0700

    game class

--- Logic/Game.cs
+++ Logic/Game.cs
@@ -1,8 +1,26 @@
 ﻿public class Game
 {
-    public GameStatus Status {get; private set;}
-    public string Name {get;set;}
-    public int NumberOfPlayer{get;set;}
+    public GameStatus Status { get; private set; }
+    public string Name { get; set; }
+    public int NumberOfPlayer { get; set; }
+
+
+    public static Game Instance { get; private set; } = new Game();
+    public Player Player1 { get; private set; }
+    public Player Player2 { get; private set; }
+    public List<Tile> Board { get; private set; }
+    public bool NoOneCanPlay
+    {
+        get
+        {
+            if (Player1 is null || Player2 is null)
+                return false;
+
+            var player1CanPlay = Player1.HasMatchFor(Board.Last());
+            var player2CanPlay = Player2.HasMatchFor(Board.Last());
+            return !player1CanPlay && !player2CanPlay;
+        }
+    }
     public Game()
     {
         Status = GameStatus.Not_Started;
